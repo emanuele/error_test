03d278c3010a718343fbd0441250456e6c8c96fd
Emanuele Olivetti <olivetti@fbk.eu>
2014-07-24 14:09:16 +0200

    mass renaming C1->E1, C2->E2 + enabled larger example

--- simple.py
+++ simple.py
@@ -62,29 +62,29 @@ if __name__ == '__main__':
     iterations = 50000
 
     # Example 1:
-    # C1 = np.array([[0.31, 0.31, 0.19, 0.19],
-    #                [0.06, 0.81, 0.00, 0.13],
-    #                [.13 , 0.0, .81, .06],
-    #                [.25, .13, .25, .38]])
+    C1 = np.array([[0.31, 0.31, 0.19, 0.19],
+                   [0.06, 0.81, 0.00, 0.13],
+                   [.13 , 0.0, .81, .06],
+                   [.25, .13, .25, .38]])
 
-    # C1 = np.round(C1 * 16).astype(np.int)
+    C1 = np.round(C1 * 16).astype(np.int)
 
-    # C2 = np.array([[0.4, 0.3, 0.19, 0.11],
-    #                [0.3, 0.41, 0.13, 0.16],
-    #                [.17 , 0.11, .45, .26],
-    #                [.15, .23, .22, .39]])
+    C2 = np.array([[0.4, 0.3, 0.19, 0.11],
+                   [0.3, 0.41, 0.13, 0.16],
+                   [.17 , 0.11, .45, .26],
+                   [.15, .23, .22, .39]])
 
-    # C2 = np.round(C2 * 16).astype(np.int)
+    C2 = np.round(C2 * 16).astype(np.int)
 
 
     # Example 2: C1 and C2 have different patterns
-    C1 = np.array([[1, 6, 5],
-                   [1, 9, 5],
-                   [8, 3, 1]])
+    # C1 = np.array([[1, 6, 5],
+    #                [1, 9, 5],
+    #                [8, 3, 1]])
 
-    C2 = np.array([[6, 5, 1],
-                   [8, 6, 1],
-                   [2, 4, 6]])
+    # C2 = np.array([[6, 5, 1],
+    #                [8, 6, 1],
+    #                [2, 4, 6]])
 
     # Example 3: C1 and C2 have pretty patterns
     # C1 = np.array([[5, 6, 1],
@@ -107,10 +107,10 @@ if __name__ == '__main__':
     n_classes = C1.shape[0]
 
     alpha = np.ones(n_classes - 1)
-    total1 = C1.sum(1) - C1.diagonal()
-    total2 = C2.sum(1) - C2.diagonal()
-    C1 = C1[np.eye(n_classes)==0].reshape(n_classes, n_classes-1)
-    C2 = C2[np.eye(n_classes)==0].reshape(n_classes, n_classes-1)
+    E1 = C1[np.eye(n_classes)==0].reshape(n_classes, n_classes-1)
+    E2 = C2[np.eye(n_classes)==0].reshape(n_classes, n_classes-1)
+    total1 = E1.sum(1)
+    total2 = E2.sum(1)
 
 
     print
@@ -120,107 +120,104 @@ if __name__ == '__main__':
     counter_H2 = 0
     for i in range(iterations):
         theta = dirichlet(alpha, size=n_classes)
-        C1_candidate_H1 = np.array([multinomial(total1[j], theta[j]) for j in range(n_classes)])
-        C2_candidate_H1 = np.array([multinomial(total2[j], theta[j]) for j in range(n_classes)])
+        E1_candidate_H1 = np.array([multinomial(total1[j], theta[j]) for j in range(n_classes)])
+        E2_candidate_H1 = np.array([multinomial(total2[j], theta[j]) for j in range(n_classes)])
 
-        if (C1_candidate_H1==C1).all() and (C2_candidate_H1==C2).all():
+        if (E1_candidate_H1==E1).all() and (E2_candidate_H1==E2).all():
             counter_H1 += 1
 
         theta1 = dirichlet(alpha, size=n_classes)
         theta2 = dirichlet(alpha, size=n_classes)
-        C1_candidate_H2 = np.array([multinomial(total1[j], theta1[j]) for j in range(n_classes)])
-        C2_candidate_H2 = np.array([multinomial(total2[j], theta2[j]) for j in range(n_classes)])
+        E1_candidate_H2 = np.array([multinomial(total1[j], theta1[j]) for j in range(n_classes)])
+        E2_candidate_H2 = np.array([multinomial(total2[j], theta2[j]) for j in range(n_classes)])
 
-        if (C1_candidate_H2==C1).all() and (C2_candidate_H2==C2).all():
+        if (E1_candidate_H2==E1).all() and (E2_candidate_H2==E2).all():
             counter_H2 += 1
 
     print "counter_H1:", counter_H1
     print "counter_H2:", counter_H2
 
-    p_C1C2_given_H1 = counter_H1 / float(iterations)
-    p_C1C2_given_H2 = counter_H2 / float(iterations)
-    print "Approximate p(C1,C2 | H1):", p_C1C2_given_H1
-    print "Approximate p(C1,C2 | H2):", p_C1C2_given_H2    
+    p_E1E2_given_H1 = counter_H1 / float(iterations)
+    p_E1E2_given_H2 = counter_H2 / float(iterations)
+    print "Approximate p(E1,E2 | H1):", p_E1E2_given_H1
+    print "Approximate p(E1,E2 | H2):", p_E1E2_given_H2
 
     print
     print "Now, a more accurate MonteCarlo sampling based on multinomial_pmf (%d iterations)." % iterations
-    p_C1C2_given_H1 = 0.0
-    p_C1C2_given_H2 = 0.0
+    p_E1E2_given_H1 = 0.0
+    p_E1E2_given_H2 = 0.0
     np.random.seed(seed) # for reproducibility
     for i in range(iterations):
         theta = dirichlet(alpha, size=n_classes)
-        tmp = np.prod([multinomial_pmf(C1[j], theta[j]) for j in range(n_classes)])
-        tmp *= np.prod([multinomial_pmf(C2[j], theta[j]) for j in range(n_classes)])
-        p_C1C2_given_H1 += tmp
+        tmp = np.prod([multinomial_pmf(E1[j], theta[j]) for j in range(n_classes)])
+        tmp *= np.prod([multinomial_pmf(E2[j], theta[j]) for j in range(n_classes)])
+        p_E1E2_given_H1 += tmp
 
         theta1 = dirichlet(alpha, size=n_classes)
         theta2 = dirichlet(alpha, size=n_classes)
-        tmp = np.prod([multinomial_pmf(C1[j], theta1[j]) for j in range(n_classes)])
-        tmp *= np.prod([multinomial_pmf(C2[j], theta2[j]) for j in range(n_classes)])
-        p_C1C2_given_H2 += tmp
+        tmp = np.prod([multinomial_pmf(E1[j], theta1[j]) for j in range(n_classes)])
+        tmp *= np.prod([multinomial_pmf(E2[j], theta2[j]) for j in range(n_classes)])
+        p_E1E2_given_H2 += tmp
 
         
-    p_C1C2_given_H1 = p_C1C2_given_H1 / float(iterations)
-    p_C1C2_given_H2 = p_C1C2_given_H2 / float(iterations)
-    print "Approximate p(C1,C2 | H1):", p_C1C2_given_H1
-    print "Approximate p(C1,C2 | H2):", p_C1C2_given_H2    
-    print "Bayes factor 1/2 :", p_C1C2_given_H1 / p_C1C2_given_H2
-    print "Bayes factor 2/1 :", p_C1C2_given_H2 / p_C1C2_given_H1    
+    p_E1E2_given_H1 = p_E1E2_given_H1 / float(iterations)
+    p_E1E2_given_H2 = p_E1E2_given_H2 / float(iterations)
+    print "Approximate p(E1,E2 | H1):", p_E1E2_given_H1
+    print "Approximate p(E1,E2 | H2):", p_E1E2_given_H2
+    print "Bayes factor 1/2 :", p_E1E2_given_H1 / p_E1E2_given_H2
+    print "Bayes factor 2/1 :", p_E1E2_given_H2 / p_E1E2_given_H1
 
 
     print
     print "Now a MUCH faster implementation using vectorized code (%d iterations)." % iterations
     np.random.seed(seed) # for reproducibility
     theta = dirichlet(alpha, size=(iterations, n_classes))
-    p_C1C2_given_H1 = np.prod([multinomial_pmf_vectorized(C1[j], theta[:,j,:]) for j in range(n_classes)], 0)
-    p_C1C2_given_H1 *= np.prod([multinomial_pmf_vectorized(C2[j], theta[:,j,:]) for j in range(n_classes)], 0)
+    p_E1E2_given_H1 = np.prod([multinomial_pmf_vectorized(E1[j], theta[:,j,:]) for j in range(n_classes)], 0)
+    p_E1E2_given_H1 *= np.prod([multinomial_pmf_vectorized(E2[j], theta[:,j,:]) for j in range(n_classes)], 0)
 
     theta1 = dirichlet(alpha, size=(iterations, n_classes))
     theta2 = dirichlet(alpha, size=(iterations, n_classes))
-    p_C1C2_given_H2 = np.prod([multinomial_pmf_vectorized(C1[j], theta1[:,j,:]) for j in range(n_classes)], 0)
-    p_C1C2_given_H2 *= np.prod([multinomial_pmf_vectorized(C2[j], theta2[:,j,:]) for j in range(n_classes)], 0)
+    p_E1E2_given_H2 = np.prod([multinomial_pmf_vectorized(E1[j], theta1[:,j,:]) for j in range(n_classes)], 0)
+    p_E1E2_given_H2 *= np.prod([multinomial_pmf_vectorized(E2[j], theta2[:,j,:]) for j in range(n_classes)], 0)
 
-    p_C1C2_given_H1 = p_C1C2_given_H1.mean()
-    p_C1C2_given_H2 = p_C1C2_given_H2.mean()
-    print "Approximate p(C1,C2 | H1):", p_C1C2_given_H1
-    print "Approximate p(C1,C2 | H2):", p_C1C2_given_H2
-    print "Bayes factor 1/2 :", p_C1C2_given_H1 / p_C1C2_given_H2
-    print "Bayes factor 2/1 :", p_C1C2_given_H2 / p_C1C2_given_H1
-
+    p_E1E2_given_H1 = p_E1E2_given_H1.mean()
+    p_E1E2_given_H2 = p_E1E2_given_H2.mean()
+    print "Approximate p(E1,E2 | H1):", p_E1E2_given_H1
+    print "Approximate p(E1,E2 | H2):", p_E1E2_given_H2
+    print "Bayes factor 1/2 :", p_E1E2_given_H1 / p_E1E2_given_H2
+    print "Bayes factor 2/1 :", p_E1E2_given_H2 / p_E1E2_given_H1
 
 
     print
     print "Now a MUCH faster implementation using vectorized code in log space (%d iterations)." % iterations
     np.random.seed(seed) # for reproducibility
     theta = dirichlet(alpha, size=(iterations, n_classes))
-    log_p_C1C2_given_H1 = np.sum([log_multinomial_pmf_vectorized(C1[j], theta[:,j,:]) for j in range(n_classes)], 0)
-    log_p_C1C2_given_H1 += np.sum([log_multinomial_pmf_vectorized(C2[j], theta[:,j,:]) for j in range(n_classes)], 0)
+    log_p_E1E2_given_H1 = np.sum([log_multinomial_pmf_vectorized(E1[j], theta[:,j,:]) for j in range(n_classes)], 0)
+    log_p_E1E2_given_H1 += np.sum([log_multinomial_pmf_vectorized(E2[j], theta[:,j,:]) for j in range(n_classes)], 0)
 
     theta1 = dirichlet(alpha, size=(iterations, n_classes))
     theta2 = dirichlet(alpha, size=(iterations, n_classes))
-    log_p_C1C2_given_H2 = np.sum([log_multinomial_pmf_vectorized(C1[j], theta1[:,j,:]) for j in range(n_classes)], 0)
-    log_p_C1C2_given_H2 += np.sum([log_multinomial_pmf_vectorized(C2[j], theta2[:,j,:]) for j in range(n_classes)], 0)
+    log_p_E1E2_given_H2 = np.sum([log_multinomial_pmf_vectorized(E1[j], theta1[:,j,:]) for j in range(n_classes)], 0)
+    log_p_E1E2_given_H2 += np.sum([log_multinomial_pmf_vectorized(E2[j], theta2[:,j,:]) for j in range(n_classes)], 0)
 
-    p_C1C2_given_H1 = np.exp(log_p_C1C2_given_H1).mean()
-    p_C1C2_given_H2 = np.exp(log_p_C1C2_given_H2).mean()
-    print "Approximate p(C1,C2 | H1):", p_C1C2_given_H1
-    print "Approximate p(C1,C2 | H2):", p_C1C2_given_H2
-    print "Bayes factor 1/2 :", p_C1C2_given_H1 / p_C1C2_given_H2
-    print "Bayes factor 2/1 :", p_C1C2_given_H2 / p_C1C2_given_H1
+    p_E1E2_given_H1 = np.exp(log_p_E1E2_given_H1).mean()
+    p_E1E2_given_H2 = np.exp(log_p_E1E2_given_H2).mean()
+    print "Approximate p(E1,E2 | H1):", p_E1E2_given_H1
+    print "Approximate p(E1,E2 | H2):", p_E1E2_given_H2
+    print "Bayes factor 1/2 :", p_E1E2_given_H1 / p_E1E2_given_H2
+    print "Bayes factor 2/1 :", p_E1E2_given_H2 / p_E1E2_given_H1
 
 
     print
     print "...and now... the EXACT computation!"
-    from multivariate_polya import multivariate_polya, log_multivariate_polya_vectorized
-
     def coeff(x):
         return gamma(x.sum() + 1.0) / gamma(x + 1).prod()
 
-    p_C1C2_given_H1 = np.prod([coeff(C1[i]) * coeff(C2[i]) / coeff(C1[i] + C2[i]) * multivariate_polya(C1[i] + C2[i], alpha) for i in range(n_classes)])
-    p_C1C2_given_H2 = np.prod([multivariate_polya(C1[i], alpha) * multivariate_polya(C2[i], alpha) for i in range(n_classes)])
+    p_E1E2_given_H1 = np.prod([coeff(E1[i]) * coeff(E2[i]) / coeff(E1[i] + E2[i]) * multivariate_polya(E1[i] + E2[i], alpha) for i in range(n_classes)])
+    p_E1E2_given_H2 = np.prod([multivariate_polya(E1[i], alpha) * multivariate_polya(E2[i], alpha) for i in range(n_classes)])
 
-    print "Exact p(C1,C2 | H1):", p_C1C2_given_H1
-    print "Exact p(C1,C2 | H2):", p_C1C2_given_H2
-    print "Bayes factor 1/2 :", p_C1C2_given_H1 / p_C1C2_given_H2
-    print "Bayes factor 2/1 :", p_C1C2_given_H2 / p_C1C2_given_H1
+    print "Exact p(E1,E2 | H1):", p_E1E2_given_H1
+    print "Exact p(E1,E2 | H2):", p_E1E2_given_H2
+    print "Bayes factor 1/2 :", p_E1E2_given_H1 / p_E1E2_given_H2
+    print "Bayes factor 2/1 :", p_E1E2_given_H2 / p_E1E2_given_H1
 
